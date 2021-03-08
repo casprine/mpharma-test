33015f4686ed71721a531431bfd1014c58ff19b1
Casprine Assempah <casprine.001@gmail.com>
2021-03-08 00:55:53 +0000

    implement create product functionality

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -6,15 +6,13 @@ const initialState = {
 
 const ACTIONS = {
   [INIT_LOAD]: (state, action) => {
-    return { ...state, ...action.data };
+    return {
+      ...action.data,
+      ...state,
+    };
   },
   [ADD_ITEM]: (state, action) => {
     const { name, price } = action.data;
-
-    console.log({ name, price, action });
-
-    return { ...state };
-
     const newProductId = Math.max(...state.products.map((p) => p.id)) + 1;
     const updatedProducts = [...state.products, { id: newProductId, name }];
     const updatedPrices = [
